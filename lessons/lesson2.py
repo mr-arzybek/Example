dafb9f9cd5a31693c99d1d562a99e440941d1308
mr-arzybek <abdyrakhmanov.arzybek@gmail.com>
2024-12-05 08:34:20 +0600

    tt

--- lessons/lesson2.py
+++ lessons/lesson2.py
@@ -4,7 +4,7 @@
 # import lesson1
 # arzy = lesson1.Person("Арзы", 25, "Бишкек")
 # arzy.introduce()
-#
+
 # from lesson1 import Person
 # arzy = Person("Арзы", 25, "Бишкек")
 # arzy.introduce()
@@ -29,24 +29,45 @@
 
 # Базовый\Супер\Родительский\
 class Hero:
-    def __init__(self, name, health):
+    def __init__(self, name, hp):
         self.name = name
-        self.health = health
+        self.hp = hp
 
     def introduce(self):
-        return f"Я {self.name}, мой уровень здоровья: {self.health}"
+        return f"Я {self.name}, мой уровень здоровья: {self.hp}"
 
     def rest(self):
-        self.health += 10
-        return f"{self.name} отдыхает и восстанавливает здоровье. Новое здоровье: {self.health}"
+        self.hp += 10
+        return f"{self.name} отдыхает и восстанавливает здоровье. Новое здоровье: {self.hp}"
 
     def action(self):
         return f"{self.name} выполняет базовое действие.\n"
 
+class Warrior(Hero):
+    # pass
+    def __init__(self, name, hp, st):
+        super().__init__(name, hp)
+        self.st = st
+
+    def attack(self):
+        if self.st >= 10:
+            self.st -= 10
+            return f"{self.name} Атакует!"
+        else:
+            return f"у {self.name} не хватает стамины"
+
+
+
+warrior = Warrior("Kirito", 100, 100)
+
+print(warrior.rest())
+print(warrior.action())
+print(warrior.attack())
+
 # Класс Mage, наследующийся от Hero
 class Mage(Hero):
-    def __init__(self, name, health, mana=100):
-        super().__init__(name, health)
+    def __init__(self, name, hp, mana=100):
+        super().__init__(name, hp)
         self.mana = mana
 
     def cast_spell(self):
@@ -56,19 +77,21 @@ class Mage(Hero):
         else:
             return f"{self.name} недостаточно маны для заклинания!"
 
+    # Расширение метода(Method Extension)
+    # Вызов метода родителя(Calling Parent Method)
     def action(self):
         base_action = super().action()
         spell_result = self.cast_spell()  # Вызов метода внутри другого метода
         return f"{base_action} {spell_result}"
 
-def hero_action(hero):
-    print(hero.introduce())
-    print(hero.rest())
-    print(hero.action())
-
-mage = Mage("Гэндальф", 100, 50)
-
-hero_action(mage)
+# def hero_action(hero):
+#     print(hero.introduce())
+#     print(hero.rest())
+#     print(hero.action())
+#
+# mage = Mage("Гэндальф", 100, 50)
+#
+# hero_action(mage)
 
 # # Класс Tank, наследующийся от Hero
 # class Tank(Hero):
@@ -117,8 +140,55 @@ hero_action(mage)
 
 # gitignore
 
-
-
+#
+# class Hero:
+#     def __init__(self, name, level, hp, mp):
+#         self.name = name
+#         self.level = level
+#         self.hp = hp
+#         self.mp = mp
+#
+#
+#     def attack(self):
+#         """Метод атаки, который будет переопределен в дочерних классах."""
+#         return f"{self.name} атакует!"
+#
+#     def __str__(self):
+#         """Возвращает строковое представление героя."""
+#         return f"Герой: {self.name}, Уровень: {self.level}"
+#
+#
+# class Warrior(Hero):
+#     def attack(self):
+#         """Воин атакует с использованием меча."""
+#         return f"{self.name} наносит мощный удар мечом!"
+#
+#
+# class Mage(Hero):
+#     def attack(self):
+#         """Маг атакует с использованием магии."""
+#         return f"{self.name} запускает огненный шар!"
+#
+#
+# class Archer(Hero):
+#     def attack(self):
+#         """Лучник атакует с использованием лука."""
+#         return f"{self.name} стреляет из лука!"
+#
+#
+# # Создаем разных героев
+# warrior = Warrior("Артур", 10)
+# mage = Mage("Мерлин", 15)
+# archer = Archer("Робин", 12)
+#
+# # Список героев
+# heroes = [warrior, mage, archer]
+#
+# # Вызываем метод attack для каждого героя
+# for hero in heroes:
+#     print(hero)  # Полиморфизм: все герои обрабатываются одинаково
+#     print(hero.attack())  # Вызывается переопределенный метод attack
+#     print()
 
 
 
